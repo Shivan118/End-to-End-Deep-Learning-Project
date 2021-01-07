590880fdd8d81ad45946030d1f845bed33da9b48
SHIVAN KUMAR <51358670+Shivan118@users.noreply.github.com>
2021-01-07 11:32:19 +0530

    Add files via upload

---
+++ images.py
@@ -0,0 +1,70 @@
+import os
+import requests 
+from bs4 import BeautifulSoup 
+
+
+Google_Image = 'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
+
+
+u_agnt = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive',
+} #write: 'my user agent' in browser to get your browser user agent details
+
+Image_Folder = 'Hundai'
+
+def main():
+    if not os.path.exists(Image_Folder):  
+        os.mkdir(Image_Folder)
+    download_images()
+
+def download_images():
+    data = input('Enter your search keyword: ')
+    num_images = int(input('Enter the number of images you want: '))
+    
+    print('Searching Images....')
+    
+    search_url = Google_Image + 'q=' + data 
+    
+    
+    response = requests.get(search_url, headers=u_agnt)
+    html = response.text 
+    
+    
+    b_soup = BeautifulSoup(html, 'html.parser') 
+    results = b_soup.findAll('img', {'class': 'rg_i Q4LuWd'})
+    
+    
+    count = 0
+    imagelinks= []
+    for res in results:
+        try:
+            link = res['data-src']
+            imagelinks.append(link)
+            count = count + 1
+            if (count >= num_images):
+                break
+            
+        except KeyError:
+            continue
+    
+    print(f'Found {len(imagelinks)} images')
+    print('Start downloading...')
+
+    for i, imagelink in enumerate(imagelinks):
+        # open each image link and save the file
+        response = requests.get(imagelink)
+        
+        imagename = Image_Folder + '/' + data + str(i+1) + '.jpg'
+        with open(imagename, 'wb') as file:
+            file.write(response.content)
+
+    print("Images download completed")
+
+if __name__=='__main__':
+    main()
+
